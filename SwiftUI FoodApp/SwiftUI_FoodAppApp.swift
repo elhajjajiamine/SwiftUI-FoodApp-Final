e24b48f11fda7f887da25caaf032d987213719f6
elhajjajiamine <aminselhajjaji@gmail.com>
2020-10-28 10:08:35 +0100

    Initial Commit

---
+++ SwiftUI FoodApp/SwiftUI_FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FoodAppApp.swift
+//  SwiftUI FoodApp
+//
+//  Created by elhajjaji on 10/28/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
